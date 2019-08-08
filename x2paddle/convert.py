7ff7232eaa4ba25a1307fbee7fb7d8d0c7f98998
channingss <chen_linghchi@163.com>
2019-08-08 16:17:20 +0800

    resovle conflict of convert.py

--- x2paddle/convert.py
+++ x2paddle/convert.py
@@ -90,24 +90,6 @@ def tf2paddle(model_path, save_dir):
 
 
 def caffe2paddle(proto, weight, save_dir, caffe_proto):
-    if caffe_proto is not None:
-        import os
-        if not os.path.isfile(caffe_proto + 'caffe_pb2.py'):
-            print("The file that resolve caffe is not exist.")
-            return
-    else:
-        try:
-            import caffe
-            version = caffe.__version__
-            if version != '1.0.0':
-                print("caffe == 1.0.0 is required")
-                return
-        except:
-            print("Caffe is not installed.")
-            print(
-                "You have 2 options: 1. install caffe 2. compile the caffe.proto"
-            )
-            return
     from x2paddle.decoder.caffe_decoder import CaffeDecoder
     from x2paddle.op_mapper.caffe_op_mapper import CaffeOpMapper
 
