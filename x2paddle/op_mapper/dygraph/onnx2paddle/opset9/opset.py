92847d0b8180f0ab89cc0b14236b94c139c9c1ad
Channingss <chen_lingchi@163.com>
2021-01-19 06:23:53 +0000

    add comment of function:_rename_or_remove_weight

--- x2paddle/op_mapper/dygraph/onnx2paddle/opset9/opset.py
+++ x2paddle/op_mapper/dygraph/onnx2paddle/opset9/opset.py
@@ -48,8 +48,10 @@ def _rename_or_remove_weight(weights, origin_name, target_name=None, is_remove=T
 
     Args:
         weights(dict[String:np.ndarray]): Dict stored paramters, the key in weights is name of parameter.
-        origin_name(String): Name of parameter to eename or remove.
-        target_name(String, optional): if target_name is not None, add new key-value pair {target_name:weights[origin_name]} to weights, and target_name must follow paddle's naming rule of parameters. Default: None.
+        origin_name(String): Name of parameter to rename or remove.
+        target_name(String, optional): if target_name is not None, add new key-value pair 
+            {target_name:weights[origin_name]} to weights, and target_name must follow paddle's 
+            naming rule of parameters. Default: None.
         is_remove: if is_remove is True, remove origin key-value pair. Default: True.
     Returns:
         None
