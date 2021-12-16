abe16eac2b5ea22f4ac5c4ce567801961ad612b4
wjj19950828 <wjjisloser@163.com>
2021-12-16 17:38:45 +0800

    fixed for CI

--- x2paddle/op_mapper/onnx2paddle/opset9/opset.py
+++ x2paddle/op_mapper/onnx2paddle/opset9/opset.py
@@ -348,7 +348,7 @@ class OpSet9():
                         outputs=[node.name],
                         axis=0)
                 else:
-                    if len(size_values) > 0:
+                    if  size_values is not None:
                         attrs["size"] = [size_values[2], size_values[3]]
                     else:
                         var_nc, var_hw = val_sizes.name + '_nc', val_sizes.name + '_hw'
