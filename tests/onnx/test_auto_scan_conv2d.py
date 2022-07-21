572bbf9aab439b39077f7cf216a3d62e4d9ad07b
wjj19950828 <wjjisloser@163.com>
2022-07-21 16:56:29 +0800

    rename conv test

--- tests/onnx/test_auto_scan_conv2d.py
+++ tests/onnx/test_auto_scan_conv2d.py
@@ -22,12 +22,10 @@ import numpy as np
 import unittest
 
 
-class TestConvConvert(OPConvertAutoScanTest):
+class TestConv2dConvert(OPConvertAutoScanTest):
     """
-    api: paddle.nn.Conv2d
-    OPset version: 9
-    1.OPset version需要根据op_mapper中定义的version来设置。
-    2.测试中所有OP对应升级到Opset version 15。
+    ONNX op: Conv
+    OPset version: 7, 9
     """
 
     def sample_convert_config(self, draw):
