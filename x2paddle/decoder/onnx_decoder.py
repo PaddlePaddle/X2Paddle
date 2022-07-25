c9f42f5584f7611202129869c8d3415b32aeb3e9
qqj1130247885 <51647379+qqj1130247885@users.noreply.github.com>
2022-07-25 14:57:25 +0800

    Update onnx_decoder.py

--- x2paddle/decoder/onnx_decoder.py
+++ x2paddle/decoder/onnx_decoder.py
@@ -335,13 +335,13 @@ class ONNXGraph(Graph):
                                             node.which_child[new_nd_name] = idx
                                             break
                             else:
+                                first_i = node.inputs.index(nd.name)
                                 # deal with Multiple outputs correspond to one node
                                 if self.node_map[nd.name].outputs.count(layer_name) > 1:
                                     new_child_name = "{}/{}".format(nd.name, idx)
                                     node.which_child[new_child_name] = idx
                                 else:
                                     node.which_child[nd.name] = idx
-                                first_i = node.inputs.index(nd.name)
                             self.node_map[nd.name].index = 0
                             break
                     if flag == 1:
