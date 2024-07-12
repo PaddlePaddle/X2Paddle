14d4008a11f18e9776addd9f63e9c663f6aa026e
Tao Luo <luotao02@baidu.com>
2024-07-12 17:55:13 +0800

    add pytorch2paddle: SwinTransformer (#1027)

--- test_benchmark/PyTorch/convert.sh
+++ test_benchmark/PyTorch/convert.sh
@@ -3,6 +3,13 @@ if [ ! -d "dataset/" ]; then
   wget -nc https://x2paddle.bj.bcebos.com/test_benchmark/PyTorch/dataset.tar.gz
   tar xzvf dataset.tar.gz
 fi
+# 后续打包进 dataset.tar.gz
+if [ ! -d "dataset/SwinTransformer" ]; then	
+  cd dataset
+  wget -nc https://x2paddle.bj.bcebos.com/test_benchmark/PyTorch/SwinTransformer.tar.gz
+  tar xzvf SwinTransformer.tar.gz
+  cd ..
+fi
 find . -name "result.txt" | xargs rm -rf
 find . -name "pd_model" | xargs rm -rf 
 find . -name "pd_model_trace" | xargs rm -rf 
