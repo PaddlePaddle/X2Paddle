37dcbf13d297d604c79e8e6fb48c979b4ab0d25e
megemini <megemini@outlook.com>
2024-10-13 19:41:05 +0800

    [Fix] pytorch edsr

--- test_benchmark/PyTorch/EDSR/pd_infer.py
+++ test_benchmark/PyTorch/EDSR/pd_infer.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import paddle.fluid as fluid
+
 import paddle
 import sys
 import os
@@ -13,11 +13,8 @@ try:
     # trace
     paddle.enable_static()
     exe = paddle.static.Executor(paddle.CPUPlace())
-    [prog, inputs, outputs] = fluid.io.load_inference_model(
-        dirname="pd_model_trace/inference_model/",
-        executor=exe,
-        model_filename="model.pdmodel",
-        params_filename="model.pdiparams")
+    [prog, inputs, outputs] = paddle.static.load_inference_model(
+        path_prefix="pd_model_trace/inference_model/model", executor=exe)
     result = exe.run(prog, feed={inputs[0]: img}, fetch_list=outputs)
     df = pytorch_output - result
     if np.max(np.fabs(df)) > 1e-03:
@@ -28,11 +25,8 @@ try:
     # script
     paddle.enable_static()
     exe = paddle.static.Executor(paddle.CPUPlace())
-    [prog, inputs, outputs] = fluid.io.load_inference_model(
-        dirname="pd_model_script/inference_model/",
-        executor=exe,
-        model_filename="model.pdmodel",
-        params_filename="model.pdiparams")
+    [prog, inputs, outputs] = paddle.static.load_inference_model(
+        path_prefix="pd_model_script/inference_model/model", executor=exe)
     result = exe.run(prog, feed={inputs[0]: img}, fetch_list=outputs)
     df = pytorch_output - result
     if np.max(np.fabs(df)) > 1e-03:
