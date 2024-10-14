b12378e99dedc6d333deea383f104c5ea109ca9d
megemini <megemini@outlook.com>
2024-10-14 18:55:00 +0800

    [Fix] tensorflow frozen mobilenetv1

--- test_benchmark/TensorFlow/frozen_MobileNetV1/pd_infer.py
+++ test_benchmark/TensorFlow/frozen_MobileNetV1/pd_infer.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import paddle
-import paddle.fluid as fluid
 import sys
 import os
 import numpy as np
@@ -14,11 +13,9 @@ exe = paddle.static.Executor(paddle.CPUPlace())
 
 input_data = np.random.rand(1, 224, 224, 3).astype("float32")
 # test dygrah
-[inference_program, feed_target_names, fetch_targets
- ] = fluid.io.load_inference_model(dirname="pd_model_dygraph/inference_model/",
-                                   executor=exe,
-                                   model_filename="model.pdmodel",
-                                   params_filename="model.pdiparams")
+[inference_program, feed_target_names,
+ fetch_targets] = paddle.static.load_inference_model(
+     path_prefix="pd_model_dygraph/inference_model/model", executor=exe)
 
 result = exe.run(inference_program,
                  feed={feed_target_names[0]: input_data},
