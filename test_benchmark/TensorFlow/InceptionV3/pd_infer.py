354c1c330478ff7b039c6bb251da740ae7905261
megemini <megemini@outlook.com>
2024-10-14 15:19:59 +0800

    [Fix] tensorflow inceptionv3

--- test_benchmark/TensorFlow/InceptionV3/pd_infer.py
+++ test_benchmark/TensorFlow/InceptionV3/pd_infer.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import paddle
-import paddle.fluid as fluid
 import sys
 import os
 import numpy as np
@@ -17,11 +16,9 @@ try:
 
     # test dygrah
     [inference_program, feed_target_names,
-     fetch_targets] = fluid.io.load_inference_model(
-         dirname="pd_model_dygraph/inference_model/",
-         executor=exe,
-         model_filename="model.pdmodel",
-         params_filename="model.pdiparams")
+     fetch_targets] = paddle.static.load_inference_model(
+         path_prefix="pd_model_dygraph/inference_model/model", executor=exe)
+
     result = exe.run(inference_program,
                      feed={feed_target_names[0]: data},
                      fetch_list=fetch_targets)
