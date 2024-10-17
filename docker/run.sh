01eb968829a652e9847e842b467c6bd3f52e258e
megemini <megemini@outlook.com>
2024-10-17 11:20:45 +0800

    Revert "[Fix] docker cuda version"
    This reverts commit 7ee86f7b8bae1077ddcf57e39f115a533bf1cfd4.

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
 #生成镜像
-docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t paddlepaddle/x2paddle:latest-dev-cuda11.2-cudnn8.2-trt8.0-gcc82 .
+docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t paddlepaddle/x2paddle:latest-dev-cuda11.8-cudnn8.6-trt8.5-gcc82 .
 
 #进入镜像
-nvidia-docker run -it --cpu-shares=20000 --name=user-x2paddle --rm -v /usr/bin/nvidia-smi:/usr/bin/nvidia-smi -v $(pwd):/workspace paddlepaddle/x2paddle:latest-dev-cuda11.2-cudnn8.2-trt8.0-gcc82 /bin/bash
+nvidia-docker run -it --cpu-shares=20000 --name=user-x2paddle --rm -v /usr/bin/nvidia-smi:/usr/bin/nvidia-smi -v $(pwd):/workspace paddlepaddle/x2paddle:latest-dev-cuda11.8-cudnn8.6-trt8.5-gcc82 /bin/bash
